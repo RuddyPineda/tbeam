ac88bffa6b8db716479386bf00bc21df84704949
RuddyPineda <ruddypineda2111@gmail.com>
2022-02-26 13:01:22 -0500

    AJUSTE cambio nombre course a index

--- views/courses.php
+++ views/courses.php
@@ -1,141 +1,142 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
-    <link rel="stylesheet" href="assets/css/styles.css">
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
+  <link rel="stylesheet" href="/assets/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+  <link rel="stylesheet" href="assets/css/styles.css">
 </head>
 
 <body>
-    
 
-<div class="container">
 
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">TECNOLOGIA BASICA EN ADULTEZ MEDIA</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav ml-auto">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="../index.php">HOME</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="about.php">about</a>
-        </li>
-        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-expanded="false">
-                            Nombre del usuario
-                            </a>
-                            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <a class="dropdown-item" href="#">Cambiar contraseña</a>
-                            <a class="dropdown-item" href="#">cerrar sesion</a>
-                            <div class="dropdown-divider"></div>
-                            <a class="dropdown-item" href="#">Something else here</a>
-                            </div>
-                        </li>
-        
-        
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
-</div>
-     <div class="container">
-   <div>
-     <hr>
-     <h2 class="d-block bg-info text-white font-italic p-2">Conociendo Gmail</h2>
-   </div>
-     </div>
-     <hr>
-     <div class="container">
+  <div class="container">
 
-      <div id="accordion" role="tablist" aria-multiselectable="true">
-        <div class="card m-2 p-3">
-          <div class="card-header" role="tab">
-             <h5>
-               <a href="#" class="text-dark" data-toggle="collapse" data-parent="#accordion" aria-expanded="true">MODULO 1: CONCEPTOS GENERALES DE GMAIL</a>
-             </h5>
-          </div>
-          <div id="" role="tabpane1" class="">
-              <div class="card-block">
-                <a href="../views/moduloUno/conociendoGmail.php">ACTIVIDAD: conociendo gmail</a>  <hr>
-                <a href="../views/moduloUno/entornoGmail.php">CONTENIDO: Diapositivas entorno Gmail</a>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">TECNOLOGIA BASICA EN ADULTEZ MEDIA</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="../index.php"><i class="fa fa-home" aria-hidden="true"></i>HOME</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="about.php">about</a>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-expanded="false">
+                Nombre del usuario
+              </a>
+              <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <a class="dropdown-item" href="#">Cambiar contraseña</a>
+                <a class="dropdown-item" href="#">cerrar sesion</a>
+                <div class="dropdown-divider"></div>
+                <a class="dropdown-item" href="#">Something else here</a>
               </div>
-           </div>
-        </div>
-         
-        <div class="card m-2 p-3">
-          <div class="card-header" role="tab">
-             <h5>
-             <a href="#"  class="text-dark" data-toggle="collapse" data-parent="#accordion" aria-expanded="true">MODULO 2: COMO CREAR TU CUENTA GMAIL </a>
-             </h5>
-          </div>
-           <div id="" role="tabpane1" class="">
-              <div class="card-block">
-                <a href="../views/moduloDos/cuentaGmail.php">CONTENIDO: diapositivas paso a paso crear tu cuenta gmail</a> <hr>
-                <a href="../views/moduloDos/contenidoVideo.php">CONTENIDO: Video interactivo tu cuenta gmail</a> <hr>
-                <a href="#">TAREA:Taller creacion de tu cuenta </a>
-              </div>
-           </div>
-        </div>
+            </li>
 
-        <div class="card m-2 p-3">
-          <div class="card-header" role="tab">
-            <h5>
-            <a href="#"  class="text-dark" data-toggle="collapse" data-parent="#accordion" aria-expanded="true">MODULO 3: COMO PERSONALIZAR TU CUENTA GMAIL</a>
-            </h5>
-          </div>
-          <div id="" role="tabpane1" class="">
-              <div class="card-block">
-                <a href="../views/modulothree/personalizacionGmail.php">CONTENIDO: Diapositivas personalizar y editar tu cuenta Gmil</a>
-              </div>
-           </div>
-        </div>
 
-        <hr> <hr>
-        <div class="card m-2 p-3">
-          <div class="card-header" role="tab">
-            <h5>
-            <a href="#"  class="text-dark" data-toggle="collapse" data-parent="#accordion" aria-expanded="true">CUESTIONARIOS</a>
-            </h5>
-          </div>
-          <div id="" role="tabpane1" class="">
-              <div class="card-block">
-                <a href="../views/moduloUno/cuestionaryone.php">CUESTIONARIO: Modulo 1 Conceptos generales de gmail</a> <hr>
-                <a href="../views/moduloDos/cuestionarytwo.php">CUESTIONARIO: Modulo 2 Como crear tu cuenta gmail</a> <hr>
-                <a href="../views/moduloThree/cuestionarythree.php">CUESTIONARIO: Modulo 3 Como personalizar tu cuenta Gmail</a>
-              </div>
-           </div>
-        </div>
-        <br><br><br>
-        <footer style="background:ligth; margin-top:15px">
-        <div class="row footer_father" >
-        <div id="pie">
-            <div id="centro-pie">
-                <p>
-                    (c) todos los derechos reservados Diseñado por 
-                    <a href="gmail: plataformatbeam@gmail.com" class="gmail">equipo Tbeam</a>- 2021
-                </p>
-            </div>
-
-        </div>
-        </div>
-    </footer>
-
+            </li>
+          </ul>
         </div>
       </div>
-  
-            
+    </nav>
+  </div>
+  <div class="container">
+    <div>
+      <hr>
+      <h2 class="d-block bg-info text-white font-italic p-2">Conociendo Gmail</h2>
+    </div>
+  </div>
+  <hr>
+  <div class="container">
 
+    <div id="accordion" role="tablist" aria-multiselectable="true">
+      <div class="card m-2 p-3">
+        <div class="card-header" role="tab">
+          <h5>
+            <a href="#" class="text-dark" data-toggle="collapse" data-parent="#accordion" aria-expanded="true">MODULO 1: CONCEPTOS GENERALES DE GMAIL</a>
+          </h5>
+        </div>
+        <div id="" role="tabpane1" class="">
+          <div class="card-block">
+            <a href="../views/moduloUno/conociendoGmail.php">ACTIVIDAD: conociendo gmail</a>
+            <hr>
+            <a href="../views/moduloUno/entornoGmail.php">CONTENIDO: Diapositivas entorno Gmail</a>
+          </div>
+        </div>
+      </div>
 
-        
+      <div class="card m-2 p-3">
+        <div class="card-header" role="tab">
+          <h5>
+            <a href="#" class="text-dark" data-toggle="collapse" data-parent="#accordion" aria-expanded="true">MODULO 2: COMO CREAR TU CUENTA GMAIL </a>
+          </h5>
+        </div>
+        <div id="" role="tabpane1" class="">
+          <div class="card-block">
+            <a href="../views/moduloDos/cuentaGmail.php">CONTENIDO: diapositivas paso a paso crear tu cuenta gmail</a>
+            <hr>
+            <a href="../views/moduloDos/contenidoVideo.php">CONTENIDO: Video interactivo tu cuenta gmail</a>
+            <hr>
+            <a href="#">TAREA:Taller creacion de tu cuenta </a>
+          </div>
+        </div>
+      </div>
 
+      <div class="card m-2 p-3">
+        <div class="card-header" role="tab">
+          <h5>
+            <a href="#" class="text-dark" data-toggle="collapse" data-parent="#accordion" aria-expanded="true">MODULO 3: COMO PERSONALIZAR TU CUENTA GMAIL</a>
+          </h5>
+        </div>
+        <div id="" role="tabpane1" class="">
+          <div class="card-block">
+            <a href="../views/modulothree/personalizacionGmail.php">CONTENIDO: Diapositivas personalizar y editar tu cuenta Gmil</a>
+          </div>
+        </div>
+      </div>
 
+      <hr>
+      <hr>
+      <div class="card m-2 p-3">
+        <div class="card-header" role="tab">
+          <h5>
+            <a href="#" class="text-dark" data-toggle="collapse" data-parent="#accordion" aria-expanded="true">CUESTIONARIOS</a>
+          </h5>
+        </div>
+        <div id="" role="tabpane1" class="">
+          <div class="card-block">
+            <a href="../views/moduloUno/cuestionaryone.php">CUESTIONARIO: Modulo 1 Conceptos generales de gmail</a>
+            <hr>
+            <a href="../views/moduloDos/cuestionarytwo.php">CUESTIONARIO: Modulo 2 Como crear tu cuenta gmail</a>
+            <hr>
+            <a href="../views/moduloThree/cuestionarythree.php">CUESTIONARIO: Modulo 3 Como personalizar tu cuenta Gmail</a>
+          </div>
+        </div>
+      </div>
+      <br><br><br>
+      <footer style="background:ligth; margin-top:15px">
+        <div class="row footer_father">
+          <div id="pie">
+            <div id="centro-pie">
+              <p>
+                (c) todos los derechos reservados Diseñado por
+                <a href="gmail: plataformatbeam@gmail.com" class="gmail">equipo Tbeam</a>- 2021
+              </p>
+            </div>
+
+          </div>
+        </div>
+      </footer>
+
+    </div>
+  </div>
 </body>
