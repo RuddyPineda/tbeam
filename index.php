383db019fc97c5f05d4201ac6c3a78395293fe9c
JavikRuiz <78904441+JavikRuiz@users.noreply.github.com>
2022-03-04 21:00:02 -0500

    Ajuste: unificacion

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <title>Tbeam</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
     <link rel="stylesheet" href="/assets/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="assets/css/styles.css">     
+    <link rel="stylesheet" href="/assets/css/styles.css">     
 </head>
 <body >
     <header>
@@ -16,8 +16,7 @@
             
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
-            </button>
-    
+            </button>    
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav mr-auto" style="margin:auto">
                     <li class="nav-item active">
